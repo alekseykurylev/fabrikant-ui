233977596c57e859e5da53bf6c507c3c496c230c
Aleksey Kurylev <alekseykurylev@yandex.ru>
2023-05-15 08:21:00 +0300

    init

---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+import gulp from "gulp";
+import plumber from "gulp-plumber";
+import sass from "gulp-dart-sass";
+import postcss from "gulp-postcss";
+import autoprefixer from "autoprefixer";
+import csso from "gulp-csso";
+import rename from "gulp-rename";
+import browser from "browser-sync";
+
+// Styles
+
+export const styles = () => {
+  return (
+    gulp
+      .src("src/sass/style.scss", { sourcemaps: false })
+      .pipe(plumber())
+      .pipe(sass().on("error", sass.logError))
+      .pipe(csso())
+      .pipe(rename("style.min.css"))
+      // .pipe(postcss([autoprefixer()]))
+      .pipe(gulp.dest("src/css", { sourcemaps: "." }))
+      .pipe(browser.stream())
+  );
+};
+
+// Server
+
+const server = (done) => {
+  browser.init({
+    server: {
+      baseDir: "src",
+    },
+    cors: true,
+    notify: false,
+    ui: false,
+  });
+  done();
+};
+
+// Watcher
+
+const watcher = () => {
+  gulp.watch("src/sass/**/*.scss", gulp.series(styles));
+  gulp.watch("src/*.html").on("change", browser.reload);
+};
+
+export default gulp.series(styles, server, watcher);
