3b27108cc58e8d43eeee179e88ad6b08d0c38fcc
Aleksey Kurylev <alekseykurylev@yandex.ru>
2023-06-04 17:43:16 +0300

    feat: timeline

--- src/components/timeline/timeline.js
+++ src/components/timeline/timeline.js
@@ -1,14 +1,28 @@
 (() => {
-  const timeline = document.querySelector('.uk-timeline');
-  const itemFirst = document.querySelector('.uk-timeline > li:first-child');
-  const itemLast = document.querySelector('.uk-timeline > li:last-child');
+  const container = document.querySelector('.uk-timeline-container');
+  const timeline = container.querySelector('.uk-timeline');
+  const itemFirst = timeline.querySelector('.uk-timeline > li:first-child');
+  const itemLast = timeline.querySelector('.uk-timeline > li:last-child');
 
   const qwe = () => {
-    // console.log(timeline);
+    const itemFirstWidth = itemFirst.offsetWidth - 1;
+    const itemLastWidth = itemLast.offsetWidth - 1;
+
+    if (UIkit.util.isInView(itemFirst, 0, itemFirstWidth * -1)) {
+      container.classList.remove('uk-timeline-box-shadow-left');
+    } else {
+      container.classList.add('uk-timeline-box-shadow-left');
+    }
+
+    if (UIkit.util.isInView(itemLast, 0, itemLastWidth * -1)) {
+      container.classList.remove('uk-timeline-box-shadow-right');
+    } else {
+      container.classList.add('uk-timeline-box-shadow-right');
+    }
   };
 
   qwe();
 
-  window.addEventListener('scroll', qwe);
+  timeline.addEventListener('scroll', qwe);
   window.addEventListener('resize', qwe);
 })();
