a49d7f085625205ebabf207e5ff9fb891c066e4c
Dmitry Laskin <laskin.d.a@effective-group.ru>
2024-06-27 12:40:41 +0300

    feat: replace gulp-dart-sass with gulp-sass
    * ♻️ clean gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,27 @@
 import gulp from 'gulp';
 import babel from 'gulp-babel';
 import plumber from 'gulp-plumber';
-import sass from 'gulp-dart-sass';
 import rename from 'gulp-rename';
+import sassGlob from 'gulp-sass-glob';
+import * as dartSass from 'sass';
+import gulpSass from 'gulp-sass';
 import postcss from 'gulp-postcss';
 import terser from 'gulp-terser';
 import csso from 'postcss-csso';
 import browser from 'browser-sync';
 
+const sass = gulpSass(dartSass);
 
 // UIkit Style
 
-export const uikitStyle = () => {
-  return gulp
-    .src('src/uikit/styles/sass/uikit.scss')
+export const uikitStyle = () => gulp.src('src/uikit/styles/sass/uikit.scss')
     .pipe(plumber())
+    .pipe(sassGlob())
     .pipe(sass().on('error', sass.logError))
     .pipe(postcss([csso]))
     .pipe(rename('uikit.min.css'))
     .pipe(gulp.dest('dist/uikit'))
     .pipe(browser.stream());
-};
 
 // UIkit Script
 
@@ -36,21 +37,17 @@ export const uikitScript = (done) => {
 
 // Components Style
 
-export const componentsStyle = () => {
-  return gulp
-    .src(['src/components/**/*.scss'])
+export const componentsStyle = () => gulp.src(['src/components/**/*.scss'])
     .pipe(plumber())
+    .pipe(sassGlob())
     .pipe(sass().on('error', sass.logError))
     .pipe(postcss([csso]))
     .pipe(gulp.dest('dist/components'))
     .pipe(browser.stream());
-};
 
 // Components Script
 
-export const componentsScript = () => {
-  return gulp
-    .src(['src/components/**/*.js'])
+export const componentsScript = () => gulp.src(['src/components/**/*.js'])
     .pipe(
       babel({
         presets: ['@babel/preset-env']
@@ -59,20 +56,17 @@ export const componentsScript = () => {
     .pipe(terser())
     .pipe(gulp.dest('dist/components'))
     .pipe(browser.stream());
-};
 
 // All Style
 
-export const allStyle = () => {
-  return gulp
-    .src('src/index.scss')
+export const allStyle = () => gulp.src('src/index.scss')
     .pipe(plumber())
+    .pipe(sassGlob())
     .pipe(sass().on('error', sass.logError))
     .pipe(postcss([csso]))
     .pipe(rename('style.min.css'))
     .pipe(gulp.dest('dist/css'))
     .pipe(browser.stream());
-};
 
 // Watcher
 
@@ -104,4 +98,5 @@ export const server = (done) => {
   done();
 };
 
+// Default
 export default gulp.series(server, uikitScript, uikitStyle, componentsStyle, componentsScript, allStyle, watcher);
